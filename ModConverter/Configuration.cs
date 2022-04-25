874606a9717924805a7ee7da2019633ea90f3a40
Yuki <yuuki.walsh@outlook.com>
2022-04-25 21:08:44 +1000

    initial

---
+++ ModConverter/Configuration.cs
@@ -0,0 +1,19 @@
+﻿// © Mod Converter.
+// Licensed under the MIT license.
+
+namespace ModConverter;
+
+using System;
+using Dalamud.Configuration;
+
+[Serializable]
+public class Configuration : IPluginConfiguration
+{
+	public int Version { get; set; } = 0;
+	public bool Enable { get; set; } = true;
+
+	public void Save()
+	{
+		Plugin.PluginInterface.SavePluginConfig(this);
+	}
+}
---
+++ ModConverter/Plugin.cs
@@ -0,0 +1,41 @@
+﻿// © Mod Converter.
+// Licensed under the MIT license.
+
+namespace ModConverter;
+
+using System;
+using Dalamud.Game.Command;
+using Dalamud.Game.Gui;
+using Dalamud.IoC;
+using Dalamud.Logging;
+using Dalamud.Plugin;
+
+public sealed class Plugin : IDalamudPlugin
+{
+	public Plugin()
+	{
+		try
+		{
+			Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+			////CommandManager.AddCommand((s, t) => ConfigurationInterface.Show(), "/customize", "Opens the customize+ configuration window");
+		}
+		catch (Exception ex)
+		{
+			PluginLog.Error(ex, "Error instantiating plugin");
+		}
+	}
+
+	[PluginService] [RequiredVersion("1.0")] public static DalamudPluginInterface PluginInterface { get; private set; } = null!;
+	[PluginService] [RequiredVersion("1.0")] public static CommandManager CommandManager { get; private set; } = null!;
+	[PluginService] [RequiredVersion("1.0")] public static ChatGui ChatGui { get; private set; } = null!;
+
+	public static Configuration Configuration { get; private set; } = null!;
+
+	public string Name => "Mod Converter";
+
+	public void Dispose()
+	{
+		Files.Dispose();
+		CommandManagerExtensions.Dispose();
+	}
+}
